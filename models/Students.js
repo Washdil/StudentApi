a28830494a31bfdce35d1541e30d9231e792e167
washdilBaloch <washdildm484@gmail.com>
2026-05-23 19:22:26 +0500

    created student model

---
+++ models/Students.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    age: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timeseries: true,
+  },
+);
+
+
+module.exports = mongoose.model("Student", studentSchema);
